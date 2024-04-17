33fb1bfe060a3eae1ab3af7b36074d4428698991
Aivaseda D.A <aivaseda@bk.ru>
2024-04-17 05:46:02 +0500

    fix magic number in Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -14,15 +14,19 @@ public class Progression {
     public static void startGame() {
         String task = "What number is missing in the progression?";
         String[][] firstData = new String[Engine.MAXRAUND][2];
-        String[] litllarray = new String[10];
+        var longProgression = 10;
+        var maxNumber = 40;
+        var stepProgression = 5;
+        var indexArray = 9;
+        String[] litllarray = new String[longProgression]; //magic number
         for (int i = 0; i < Engine.MAXRAUND; i++) {
-            int first = getNum(40);
-            int second = getNum(5);
+            int first = getNum(maxNumber); //magic number
+            int second = getNum(stepProgression); //magic number
             for (int x = 0; x < litllarray.length; x++) {
                 litllarray[x] = String.valueOf(first);
                 first += second;
             }
-            int point = getNum(9);
+            int point = getNum(indexArray); //magic number
             firstData[i][1] = litllarray[point];
             litllarray[point] = "..";
             firstData[i][0] = String.join(" ", litllarray);
