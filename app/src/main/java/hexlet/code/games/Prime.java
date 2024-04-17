c83606bf3ffea45af230a0b17972fba68beacec0
Aivaseda D.A <aivaseda@bk.ru>
2024-04-17 05:16:29 +0500

    update Prime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -3,11 +3,13 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Prime {
-    private static String task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    //static String task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     public static void starGame() {
+        String task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
         String[][] firstData = new String[Engine.MAXRAUND][2];
+        int maxNum = 100;
         for (int i = 0; i < Engine.MAXRAUND; i++) {
-            Integer num = getNum(100);
+            Integer num = getNum(maxNum);
             String yesno = isSimple(num) ? "yes" : "no";
             firstData[i][0] = String.valueOf(num);
             firstData[i][1] = yesno;
@@ -17,7 +19,6 @@ public class Prime {
 
     public static int getNum(Integer b) {
         Integer a = 1;
-        //Integer b = 100;
         return a + (int) (Math.random() * b);
     }
 
